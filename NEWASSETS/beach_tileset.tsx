5af264d7ceee04ecfdc1914c1af532ba78e7c7e5
xPegasus47 <33075205+xPegasus47@users.noreply.github.com>
2019-04-29 04:22:26 -0500

    Add files via upload

---
+++ NEWASSETS/beach_tileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="beach_tileset" tilewidth="16" tileheight="16" tilecount="936" columns="36">
+ <image source="../ProjectPlaceholder/trunk/beach_tileset.png" width="576" height="416"/>
+</tileset>
---
+++ NEWASSETS/tf_darkdimension_sheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.3" name="tf_darkdimension_sheet" tilewidth="16" tileheight="16" tilecount="609" columns="29">
+ <image source="../ProjectPlaceholder/trunk/tf_darkdimension_sheet.png" width="464" height="336"/>
+</tileset>
